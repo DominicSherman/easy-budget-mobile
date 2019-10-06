866860bd067fd917235615295dce72709b6de392
Nathan Thompson <nathan.thompson.re@gmail.com>
2019-10-06 13:15:15 -0500

    Use version 3.0.0.alpha.8 of RNN and change some android settings

--- android/app/src/main/java/com/easybudget/MainActivity.java
+++ android/app/src/main/java/com/easybudget/MainActivity.java
@@ -1,5 +1,7 @@
 package com.easybudget;
 
+import com.facebook.react.modules.i18nmanager.I18nUtil;
+
 import com.reactnativenavigation.NavigationActivity;
 
 public class MainActivity extends NavigationActivity {
@@ -8,7 +10,6 @@ public class MainActivity extends NavigationActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  @Override
   protected String getMainComponentName() {
     return "EasyBudget";
   }
--- android/app/src/main/java/com/easybudget/MainApplication.java
+++ android/app/src/main/java/com/easybudget/MainApplication.java
@@ -1,51 +1,50 @@
 package com.easybudget;
 
 import android.app.Application;
-import android.content.Context;
+import android.util.Log;
 
 import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
-
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
 import com.reactnativenavigation.react.ReactGateway;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class MainApplication extends NavigationApplication {
 
-    @Override
-    protected ReactGateway createReactGateway() {
-        ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
-            @Override
-            protected String getJSMainModuleName() {
-                return "index";
-            }
-        };
-        return new ReactGateway(this, isDebug(), host);
-    }
+  @Override
+  protected ReactGateway createReactGateway() {
+    ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+      @Override
+      protected String getJSMainModuleName() {
+        return "index";
+      }
+    };
+    return new ReactGateway(this, isDebug(), host);
+  }
 
-    @Override
-    public boolean isDebug() {
-        return BuildConfig.DEBUG;
-    }
+  @Override
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
+  }
 
-    protected List<ReactPackage> getPackages() {
-        // Add additional packages you require here
-        // No need to add RnnPackage and MainReactPackage
-        return Arrays.<ReactPackage>asList(
-            // eg. new VectorIconsPackage()
-        );
-    }
+  protected List<ReactPackage> getPackages() {
+    @SuppressWarnings("UnnecessaryLocalVariable")
+    List<ReactPackage> packages = new PackageList(this).getPackages();
+    // Packages that cannot be autolinked yet can be added manually here, for example:
+    // packages.add(new MyReactNativePackage());
+    return packages;
+  }
 
-    @Override
-    public List<ReactPackage> createAdditionalReactPackages() {
-        return getPackages();
-    }
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
+  }
 }
