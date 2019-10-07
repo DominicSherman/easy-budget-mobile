33b5bbfc206251647bfa0b5656c9e63a7cb0a1fa
Dominic Sherman <dominic.sherman98@gmail.com>
2019-10-07 07:48:13 -0500

    Set up react native firebase and google sign in

--- ios/EasyBudget/AppDelegate.m
+++ ios/EasyBudget/AppDelegate.m
@@ -4,12 +4,15 @@
 #import <React/RCTRootView.h>
 #import <ReactNativeNavigation/ReactNativeNavigation.h>
 
+@import Firebase;
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
   [ReactNativeNavigation bootstrap:jsCodeLocation launchOptions:launchOptions];
+  [FIRApp configure];
   
   return YES;
 }
