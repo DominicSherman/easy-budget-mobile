0898ed3325055cc21810096dc813933383b1c1d1
Dominic Sherman <dominic.sherman98@gmail.com>
2019-10-07 08:16:19 -0500

    Authenticate with iOS

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import firebase from 'react-native-firebase';
+import {
+  GoogleSigninButton,
+  GoogleSignin,
+} from '@react-native-community/google-signin';
 import {SafeAreaView} from 'react-native';
 import DefaultText from '../components/DefaultText';
 import {screenWrapper} from '../styles/shared-styles';
+
+const onPress = async () => {
+  try {
+    // add any configuration settings here:
+    await GoogleSignin.configure();
+
+    const data = await GoogleSignin.signIn();
+    console.log('data', data);
+
+    // create a new firebase credential with the token
+    const credential = firebase.auth.GoogleAuthProvider.credential(
+      data.idToken,
+      data.accessToken,
+    );
+    // login with credential
+    const firebaseUserCredential = await firebase
+      .auth()
+      .signInWithCredential(credential);
+
+    console.warn(JSON.stringify(firebaseUserCredential.user.toJSON()));
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 const Home: React.FC = () => {
   return (
     <SafeAreaView style={screenWrapper}>
       <DefaultText>{'Home'}</DefaultText>
+      <GoogleSigninButton
+        style={{width: 192, height: 48}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={onPress}
+      />
     </SafeAreaView>
   );
 };
