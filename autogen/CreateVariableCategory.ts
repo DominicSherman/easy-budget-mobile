96f289753701398b6ae721748160a904451711b0
Dominic Sherman <dominic.sherman98@gmail.com>
2019-11-25 08:41:59 -0800

    gening

--- autogen/CreateVariableCategory.ts
+++ autogen/CreateVariableCategory.ts
@@ -2,7 +2,7 @@
 /* eslint-disable */
 // This file was automatically generated and should not be edited.
 
-import { CreateVariableCategory } from "./../../../autogen/globalTypes";
+import { CreateVariableCategory } from "./globalTypes";
 
 // ====================================================
 // GraphQL mutation operation: CreateVariableCategory
---
+++ autogen/globalTypes.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export interface CreateVariableCategory {
+  variableCategoryId: string;
+  userId: string;
+  amount: number;
+  name: string;
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
