efaaf4d4b424e9ed786a98e8676f18d81fbad31c
Dominic <dominic.sherman98@gmail.com>
2020-01-27 15:05:32 -0600

    revert

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -3,13 +3,13 @@ import DefaultClient from 'apollo-client';
 
 let apolloClient: ApolloClient<any>;
 
-const LOCAL_HOST = 'http://localhost:5000/easy-budget-2f9aa/us-central1/graphql';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+const LOCAL_HOST = 'http://localhost:5000/easy-budget-2f9aa/us-central1/graphql';
 const PROD = 'https://us-central1-easy-budget-2f9aa.cloudfunctions.net/graphql';
 
 const initializeApolloClient = (): void => {
     apolloClient = new ApolloClient({
-        uri: LOCAL_HOST
+        uri: PROD
     });
 };
 
