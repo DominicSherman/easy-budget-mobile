0afbbd0d49491b8d577485f3f2ec21c17f8b3fb1
Dominic Sherman <dominic.sherman98@gmail.com>
2020-03-08 15:54:45 -0500

    Testing and stuff

--- __tests__/screens/Information.test.tsx
+++ __tests__/screens/Information.test.tsx
@@ -1,7 +1,10 @@
 import TestRenderer from 'react-test-renderer';
 import React from 'react';
+import {ScrollView} from 'react-native';
 
-import Information from '../../src/screens/Information';
+import Information, {InformationRef} from '../../src/screens/Information';
+
+jest.mock('../../src/redux/hooks');
 
 describe('Information', () => {
     let expectedProps,
@@ -14,8 +17,25 @@ describe('Information', () => {
     };
 
     beforeEach(() => {
-        expectedProps = {};
+        expectedProps = {
+            route: {
+                params: {
+                    ref: undefined
+                }
+            }
+        };
 
         render();
+    });
+
+    it('should render a scroll view when a ref is **not** passed', () => {
+        root.findByType(ScrollView);
+    });
+
+    it('should render a scroll view when a ref is passed', () => {
+        expectedProps.route.params.ref = InformationRef.EXPENSE;
+        render();
+
+        root.findByType(ScrollView);
     });
 });
--- src/components/navigation/HeaderComponents.tsx
+++ src/components/navigation/HeaderComponents.tsx
@@ -65,7 +65,7 @@ export const InfoIcon: FC = () => {
     const onPress = (): void => {
         navigation.navigate({
             name: Route.INFORMATION,
-            params: undefined
+            params: {}
         });
     };
 
