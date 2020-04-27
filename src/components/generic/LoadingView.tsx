b13dd709269a74d82ba66eb1b26c783f0c1c493a
dsherman <dsherman@hy-vee.com>
2020-04-27 12:04:15 -0700

    It is building

--- src/components/generic/LoadingView.tsx
+++ src/components/generic/LoadingView.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import LottieView from 'lottie-react-native';
 
+import animation from '../../../assets/eb-loader';
+
 export default class LoadingView extends React.PureComponent {
     render(): JSX.Element {
         return (
             <LottieView
                 autoPlay
                 loop
-                source={require('../../../assets/eb-loader.json')}
+                source={animation}
             />
         );
     }
