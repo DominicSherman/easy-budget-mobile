3c34fc2e04f29bf459d3d52951bb3d311256b33e
dsherman <dsherman@hy-vee.com>
2020-04-27 12:31:36 -0700

    This should work

--- src/components/generic/LoadingView.tsx
+++ src/components/generic/LoadingView.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import LottieView from 'lottie-react-native';
 
-import animation from '../../../assets/eb-loader';
+// eslint-disable-next-line import/extensions
+import animation from '../../../assets/eb-loader.json';
 
 export default class LoadingView extends React.PureComponent {
     render(): JSX.Element {
